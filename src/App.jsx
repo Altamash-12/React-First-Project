a9329ab7f32b09d4321cc180d7594beeefc1815a
Altamash-12 <ayank829130@gmail.com>
2024-10-12 21:32:39 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/card.jsx"
+
+
+
+function App(props) {
+  return (
+    <>
+      <Card name="ayan"/>
+    </>
+    )
+}
+
+export default App;
---
+++ src/components/card.jsx
@@ -0,0 +1,9 @@
+function Card({name}){
+    return (
+        <>
+            <h1>Hello {name}</h1> 
+        </>
+    )
+}
+
+export default Card
