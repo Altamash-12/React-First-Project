3076d2b5726a5954f68e655df87157772a3b2ac6
Altamash-12 <ayank829130@gmail.com>
2024-10-19 16:36:40 +0530

    Made it  Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,114 @@
 import { useState } from "react";
 import "./App.css";
-import Card from "./components/card.jsx"
-
-
+import TodoItem from "./components/card.jsx";
 
 function App(props) {
+  const [todos, setTodos] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const [selectedTodo, setSelectedTodo] = useState(null);
+  const [pendingTodos, setPendingTodo] = useState([]);
+  const [completedTodos, setCompletedTodo] = useState([]);
+  const [allTodos, addTodo] = useState([]);
+
+  function createTodoHandler(e) {
+    if (todoBtn.textContent === "Update todo") {
+      updateTextChange();
+      todoBtn.textContent = "Create Todo";
+    } else {
+      let todoCopy = [...todos];
+      let obj = {};
+      obj.id = Math.ceil(Math.random() * 10)
+      obj.status = "pending";
+      obj.text = inputText;
+      todoCopy.push(obj);
+      console.log(todoCopy);
+
+      setTodos(todoCopy)
+    }
+    e.preventDefault(); 
+  }
+
+  function updateTextChange() {
+    const todosCopy = [...todos];
+    todosCopy.splice(selectedTodo, 1, inputText);
+    setTodos(todosCopy);
+    setSelectedTodo(null);
+    setInputText("");
+  }
+
+  function inputChangeHandler(e) {
+    setInputText(e.target.value);
+  }
+
+  function todoDeleteHandler(index) {
+    let todoCopy = [...todos];
+    todoCopy.splice(index, 1);
+    console.log('index', index);
+    setTodos(todoCopy);
+  }
+
+  const todoBtn = document.querySelector(".createTodoBtn");
+  function updateTodo(index) {
+    setInputText(todos[index]);
+    setSelectedTodo(index);
+    todoBtn.textContent = "Update todo";
+  }
+
+  function todoCompletedHandler(e) {
+    let completedTodoCopy = [...completedTodos];
+    completedTodoCopy.push(e);
+    setCompletedTodo(completedTodoCopy);
+  }
+
+  function todoPendingHandler(e) {
+    let penndingTodoCopy = [...pendingTodos];
+    penndingTodoCopy.push(e);
+    setPendingTodo(penndingTodoCopy);
+  }
+
+  function ayan(e) {
+    if (e.target.value === "Pending") {
+     setTodos(pendingTodos)
+    } else if (e.target.value === "Completed") {
+     setTodos(completedTodos)
+    } else{
+      setTodos(allTodos)
+    }
+  }
+
   return (
-    <>
-      <Card name="ayan"/>
-    </>
-    )
+    <div className="App">
+    <h1>Create your todo list</h1>
+      <form className="todo-line" onSubmit={createTodoHandler}>
+        <input
+          onChange={(e) => inputChangeHandler(e)}
+          value={inputText}
+          placeholder="What are your tasks for today?"
+        />
+        <button className="createTodoBtn">
+          Create Todo
+        </button>
+        <select onChange={(e) => ayan(e)}>
+          <option>All</option>
+          <option>Pending</option>
+          <option>Completed</option>
+        </select>
+      </form>
+      {todos.map((todo, i) => {
+        return (
+          <TodoItem
+            key={i}
+            id={todo.id}
+            todo={todo}
+            updateTodo={updateTodo}
+            todoDeleteHandler={todoDeleteHandler}
+            todoCompletedHandler={todoCompletedHandler}
+            todoPendingHandler={todoPendingHandler}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,9 +1,31 @@
-function Card({name}){
-    return (
-        <>
-            <h1>Hello {name}</h1> 
-        </>
-    )
+function TodoItem({todo, id, todoDeleteHandler, updateTodo, todoPendingHandler, todoCompletedHandler}) {
+  function deleteHandler(index) {
+    todoDeleteHandler(index);
+}
+  // function updateHandler(todo) {
+  //  updateTodo(todo);
+  // }
+
+  // function todoPending(e){
+  //   todoPendingHandler(e)
+  // }
+
+  // function todoCompleted(e){
+  //   todoCompletedHandler(e);
+  // }
+
+  return (
+    <div className="card">
+      {todo.text && <p className="todoText">{todo.text}</p>}
+      <button className="todo-buttons">
+      <button className="edit-btn" onClick={() =>updateHandler(id)}>Edit</button>
+      <button className="delete-btn" onClick={() => deleteHandler(id)}>Delete</button>
+      <button className="pending" onClick={() =>todoPending(todo.text)}>Pending</button>
+      <button className="completed" onClick={() =>todoCompleted(todo.text)}>Completed</button>
+      </button>
+    </div>
+  );
 }
 
-export default Card
+export default TodoItem;
+
